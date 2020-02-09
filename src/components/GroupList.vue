02c56d3a811a2c0ddd700cd4b0ecf9e04be3d66a
Marvin L <contact@marvinl.com>
2020-02-09 16:22:45 -0400

    Update : component props Update : Geolocation method Update : LastPosition / address etc…

--- src/components/GroupList.vue
+++ src/components/GroupList.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
   #groupList.my-3.overflow-auto
-    b-list-group(v-for="group in groups" :key="group.id" v-show="!isLoading")
+    b-list-group(v-for="group in groupList" :key="group._id" v-show="!isLoading")
       b-list-group-item.align-items-start.mb-2(button
       v-if="group.positions.length > 0" @click="selectGroup(group)")
         .d-flex.justify-content-between
-          h5 {{group.name}}
-          span {{fromNow(group._changed)}}
+          h5.font-weight-bold {{group.name}}
+          span {{fromNow(group.positions[group.positions.length-1]._changed)}}
         p.text-left.m-0(v-if="group.address") {{group.address.display_name}}
     b-spinner(type="grow" variant="primary" v-show="isLoading")
 </template>
@@ -15,12 +15,12 @@ import { mapGetters } from 'vuex';
 
 export default {
   name: 'GroupList',
-  props: ['groups', 'handleSelect', 'fromNow'],
+  props: ['handleSelect', 'fromNow'],
   data() {
     return {};
   },
   computed: {
-    ...mapGetters(['isLoading']),
+    ...mapGetters(['isLoading', 'groupList']),
   },
   methods: {
     selectGroup(group) {
